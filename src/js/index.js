2064f79cec13a8795910047f674187c4bf411a34
KHK <ston0538@gmail.com>
2020-09-14 16:14:54 +0900

    2020년 9월호

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+const category = {
+  종합그래픽: "http://s.godo.kr/k28s",
+  "제품,가구 디자인": "http://s.godo.kr/k28u",
+  "건축/인테리어": "http://s.godo.kr/k28v",
+  "일러스트/애니메이션/케릭터": "http://s.godo.kr/k28x",
+  "아트/예술": "http://s.godo.kr/k290",
+  패션: "http://s.godo.kr/k293",
+  "BI,CI LOGO/타이포": "http://s.godo.kr/mjw6	",
+  "아트/예술": "http://s.godo.kr/k290",
+};
+console.log(category);
